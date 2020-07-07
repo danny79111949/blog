4ae67706f0a6a408b9090e07100a912658fe22f4
Danny791119 <danny79111949@gmail.com>
2020-07-07 17:59:17 +0800

    新增自己的 css js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+
 
 window.Vue = require('vue');
 
@@ -28,6 +28,4 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+
