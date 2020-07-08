e2bbbbc5381769a3e179f41c5cec6b19c9a6e68b
danny <danny79111949@gmail.com>
2020-07-08 21:28:57 +0800

    api_test

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/api_test', function (Request $request) {
+    return 'api_test';
+});
